595db56dc4aa33305799e71d9763954ee9942b88
Shubham <shubhamavhad0123@gmail.com>
2025-09-20 23:42:03 +0530

    changed photo

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -16,7 +16,7 @@ function About() {
         <div className="SectionTitle">About Me</div>
         <div className="BigCard">
           <ScrollAnimation animateIn="fadeInLeft">
-            <Image src="/mee1.png" alt="man-svgrepo" />
+            <Image src="/mee.png" alt="man-svgrepo" />
           </ScrollAnimation>
           <div className="AboutBio">
             <ScrollAnimation animateIn="fadeInLeft">
--- portfolio/src/components/Hero/Hero.js
+++ portfolio/src/components/Hero/Hero.js
@@ -94,7 +94,7 @@ function Hero() {
           <HeroRight>
             <ScrollAnimation animateIn="fadeIn">
               <Image
-                src="/mee1.png"
+                src="/mee.png"
                 alt="man-svgrepo"
               />
             </ScrollAnimation>
